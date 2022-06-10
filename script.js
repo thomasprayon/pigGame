876c9aab61e52d5bcf902f67aa0d3cb8f2dbe040
thomasprayon <thomasprayongowland@gmail.com>
2022-06-10 18:37:12 +0200

    added a initial value function and applied it to the new game button

--- script.js
+++ script.js
@@ -10,12 +10,25 @@ const score1 = document.querySelector('.score-0');
 const score2 = document.querySelector('.score-1');
 const player1 = document.querySelector('.player-0');
 const player2 = document.querySelector('.player-1');
+let activePlayer, currentScore, scores, playing;
 
-let activePlayer = 0;
-let currentScore = 0;
-let scores = [0, 0];
-let playing = true;
-player1.classList.add('current-playing');
+const initialValues = () => {
+    playing = true;
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    score1.textContent = 0;
+    score2.textContent = 0;
+    currentScore1.textContent = 0;
+    currentScore2.textContent = 0;
+    player1.classList.add('current-playing');
+    player2.classList.remove('current-playing');
+    getDice.classList.add('hidden');
+    player1.classList.remove('player-winner');
+    player2.classList.remove('player-winner');
+};
+
+initialValues();
 
 const rollDice = function () {
     return Math.floor(Math.random() * 6) + 1;
@@ -31,19 +44,20 @@ const switchPlayer = () => {
     player2.classList.toggle('current-playing');
 };
 
+function checkWinner(player) {
+    let winner, currentPlaying, topScore;
+    winner = player.classList.add('player-winner');
+    currentPlaying = player.classList.remove('current-playing');
+    return winner, currentPlaying;
+}
+
 btnRollDice.addEventListener('click', () => {
     if (playing) {
         const rollingDice = rollDice();
-
-        //Initial behaviour
         getDice.classList.remove('hidden');
         getDice.src = `/dices/dice-${rollingDice}.png`;
-
-        //What happens after
-        console.log('rollingDice', rollingDice);
         if (rollingDice >= 1) {
             currentScore += rollingDice;
-            console.log('activePlayer:', activePlayer + 1);
             if (activePlayer === 0) {
                 currentScore1.textContent = currentScore;
             } else {
@@ -60,16 +74,10 @@ btnRollDice.addEventListener('click', () => {
 btnHold.addEventListener('click', () => {
     console.log('activePLayer in Button Hold', activePlayer);
     scores[activePlayer] += currentScore;
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 100) {
         console.log('activePlayer btnHold', activePlayer);
         playing = false;
-        if (activePlayer === 0) {
-            player1.classList.remove('current-playing');
-            player1.classList.add('player-winner');
-        } else {
-            player2.classList.remove('current-playing');
-            player2.classList.add('player-winner');
-        }
+        activePlayer === 0 ? checkWinner(player1) : checkWinner(player2);
     } else if (activePlayer === 0) {
         score1.textContent = scores[activePlayer];
         switchPlayer();
@@ -77,23 +85,6 @@ btnHold.addEventListener('click', () => {
         score2.textContent = scores[activePlayer];
         switchPlayer();
     }
-
-    console.log('scores: ', (scores[activePlayer] += currentScore));
-    console.log('SCORES:', scores);
 });
 
-btnNewGame.addEventListener('click', () => {
-    playing = true;
-    scores = [0, 0];
-    currentScore = 0;
-    activePlayer = 0;
-    score1.textContent = 0;
-    score2.textContent = 0;
-    currentScore1.textContent = 0;
-    currentScore2.textContent = 0;
-    player1.classList.add('current-playing');
-    player2.classList.remove('current-playing');
-    getDice.classList.add('hidden');
-    player1.classList.remove('player-winner');
-    player2.classList.remove('player-winner');
-});
+btnNewGame.addEventListener('click', initialValues);
